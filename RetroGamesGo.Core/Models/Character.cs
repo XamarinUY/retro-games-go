8753ac310edeba6b330caa96ef701e05cef595bb
Seba Cabrera <sebastian.cabrera@horus.com.uy>
2019-02-16 14:32:32 -0300

    Added Sound library and Sounds class, mp3 & wav samples

--- RetroGamesGo.Core/Models/Character.cs
+++ RetroGamesGo.Core/Models/Character.cs
@@ -2,16 +2,27 @@
 
 namespace RetroGamesGo.Core.Models
 {
-    public class Character: EntityBase
+    public class Character : EntityBase
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public int Number { get; set; }
+        public int Year { get; set; }
         public string Url { get; set; }
         public string FunFact { get; set; }
         public string Picture { get; set; }
         public string Silhouette { get; set; }
-        public bool Captured { get; set; }
+        private bool captured;
+
+        public bool Captured
+        {
+            get => this.captured;
+            set
+            {
+                this.captured = value;
+                this.RaisePropertyChanged(() => this.Captured);
+            }
+        }
 
         public Character()
         {
