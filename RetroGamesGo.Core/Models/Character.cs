a85ff1614ce8a6ded5e6a39eb89563a1ac03174e
Seba Cabrera <sebastian.cabrera@horus.com.uy>
2019-02-16 13:17:28 -0300

    Added Property Number as int in Character

--- RetroGamesGo.Core/Models/Character.cs
+++ RetroGamesGo.Core/Models/Character.cs
@@ -4,9 +4,9 @@ namespace RetroGamesGo.Core.Models
 {
     public class Character: EntityBase
     {
-
         public string Name { get; set; }
         public string Description { get; set; }
+        public int Number { get; set; }
         public string Url { get; set; }
         public string FunFact { get; set; }
         public string Picture { get; set; }
