f379f2623104cc24bf05427589e5d09cd1f92594
Agustin Bonilla <agus.bonilla.93@hotmail.com>
2019-02-16 13:24:37 -0300

    Main view is updated

--- RetroGamesGo.Core/ViewModels/MainViewModel.cs
+++ RetroGamesGo.Core/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@
     using System.Collections;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+    using MvvmCross.Commands;
     using MvvmCross.Logging;
     using MvvmCross.Navigation;
     using RetroGamesGo.Core.Models;
@@ -15,6 +17,10 @@
     {
         public IList<Character> Characters { get; set; }
 
+        private IMvxAsyncCommand captureCommand;
+
+        public IMvxAsyncCommand CaptureCommand => captureCommand ?? (captureCommand = new MvxAsyncCommand(OnCaptureCommand, () => this.IsEnabled));
+
         /// <summary>
         /// Gets by DI the required services
         /// </summary>
@@ -25,36 +31,35 @@
                 new Character
                 {
                     Id = Guid.NewGuid(),
-                    Name = string.Empty,
-                    Image = "https://i.pinimg.com/originals/f6/32/5f/f6325fa86a0a2915d3545dc39d359e2f.png",
+                    Name = "Mario Bros",
+                    Number = 1,
+                    Description = "Es un videojuego de arcade desarrollado por Nintendo en el año 1983. Fue creado por Shigeru Miyamoto. Ha sido presentado como un minijuego en la serie de Super Mario Advance y otros juegos. Mario Bros. ha sido relanzado para Wii, Nintendo 3DS y Wii U en los servicios de Consola Virtual en Japón, Norteamérica, Europa y Australia.",
+                    Url = "https://es.wikipedia.org/wiki/Mario_Bros.",
                     FunFact = string.Empty,
-                    Unlocked = true
+                    Picture = "marioBros.png",
+                    Silhouette = "marioBrosSilhouette.png",
+                    Captured = false
                 },
                 new Character
                 {
                     Id = Guid.NewGuid(),
-                    Name = string.Empty,
-                    Image = "https://i.pinimg.com/originals/f6/32/5f/f6325fa86a0a2915d3545dc39d359e2f.png",
+                    Name = "Mario Bros",
+                    Number = 2,
+                    Description = "Es un videojuego de arcade desarrollado por Nintendo en el año 1983. Fue creado por Shigeru Miyamoto. Ha sido presentado como un minijuego en la serie de Super Mario Advance y otros juegos. Mario Bros. ha sido relanzado para Wii, Nintendo 3DS y Wii U en los servicios de Consola Virtual en Japón, Norteamérica, Europa y Australia.",
+                    Url = "https://es.wikipedia.org/wiki/Mario_Bros.",
                     FunFact = string.Empty,
-                    Unlocked = false
+                    Picture = "marioBros.png",
+                    Silhouette = "marioBrosSilhouette.png",
+                    Captured = true
                 },
-                new Character
-                {
-                    Id = Guid.NewGuid(),
-                    Name = string.Empty,
-                    Image = "https://i.pinimg.com/originals/f6/32/5f/f6325fa86a0a2915d3545dc39d359e2f.png",
-                    FunFact = string.Empty,
-                    Unlocked = false
-                },
-                new Character
-                {
-                    Id = Guid.NewGuid(),
-                    Name = string.Empty,
-                    Image = "https://i.pinimg.com/originals/f6/32/5f/f6325fa86a0a2915d3545dc39d359e2f.png",
-                    FunFact = string.Empty,
-                    Unlocked = false
-                }
             };
+        }
+
+        private async Task OnCaptureCommand()
+        {
+            this.IsBusy = true;
+            //await this.NavigationService.Navigate<CaptureViewModel>();
+            this.IsBusy = false;
         }
     }
 }
