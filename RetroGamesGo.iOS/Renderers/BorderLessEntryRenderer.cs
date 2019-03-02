2b9364b055c47de68dda9d0ecacab894045f4b6c
Diego Bonilla <diego.bonilla@nareia.com.uy>
2019-03-02 18:45:14 -0300

    compiling

---
+++ RetroGamesGo.iOS/Renderers/BorderLessEntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RetroGamesGo.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(BorderLessEntryRenderer))]
+namespace RetroGamesGo.iOS.Renderers
+{
+    public class BorderLessEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
