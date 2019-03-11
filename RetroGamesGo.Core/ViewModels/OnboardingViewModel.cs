969f3ac79846a38a34219a39084d6fea699235c8
Roygar Briceno <roygar.briceno@arkanosoft.com>
2019-03-10 23:10:51 -0300

    Fix de navegacion del onboarding

--- RetroGamesGo.Core/ViewModels/OnboardingViewModel.cs
+++ RetroGamesGo.Core/ViewModels/OnboardingViewModel.cs
@@ -1,22 +1,21 @@
 ﻿namespace RetroGamesGo.Core.ViewModels
-{
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.ComponentModel;
+{    
     using System.Threading.Tasks;
     using System.Windows.Input;
     using MvvmCross.Commands;
     using MvvmCross.Logging;
     using MvvmCross.Navigation;
-    using RetroGamesGo.Core.Helpers;
-    using RetroGamesGo.Core.Models;
-
+    using Helpers;
+ 
+    /// <summary>
+    /// Onboardirng logic showing who to playe
+    /// </summary>
     public class OnboardingViewModel : BaseViewModel
     {
         private MvxCommand play;
+
         public bool ShowLottie { get; set; } = true;
+
         public bool ShowOnboard { get; set; } = false;
 
 
@@ -24,7 +23,7 @@
         {
         }
 
-        public async override void Prepare()
+        public override async void Prepare()
         {
             if (Settings.OnboardingShown)
             {
@@ -51,8 +50,16 @@
         public ICommand Play => this.play ?? (
                this.play = new MvxCommand(async () =>
                {
-                   Settings.OnboardingShown = true;
-                   await NavigationService.Navigate<MainViewModel>();
+                   if (!Settings.OnboardingShown)
+                   {
+                       Settings.OnboardingShown = true;
+                       await NavigationService.Navigate<MainViewModel>();
+                   }
+                   else
+                   {
+                       // Second time this view appears is show, just close the ViewMode
+                       await NavigationService.Close(this);
+                   }
                }));
     }
 }
